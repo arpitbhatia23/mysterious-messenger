4696a03ea1c43478666f1e1701b7145e7d572ad9
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-10 10:28:08 +0530

    added

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,9 @@ app.use((req, res, next) => {
 import userRouter from "./routes/user.routes.js"
 import messageRouter from "./routes/message.routes.js"
 
-
+app.get("/",(req,res)=>{
+  return res.send("<h1 >server start sucessfully</h1>")
+})
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/message",messageRouter)
 
--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose"
 import { dbName } from "../constant.js"
-const dbConnect=async()=>{
+const dbConnect = async () => {
     try {
-        const connectionInstances= await mongoose.connect(`${process.env.MONGODB_URl}/${dbName}`)
-        console.log(`\n Mongodb is connected || Db host at ${connectionInstances.connection.host} `)
-
+        const connectionInstances = await mongoose.connect(`${process.env.MONGODB_URL}/${dbName}`);
+        console.log(`\n Mongodb is connected || Db host at ${connectionInstances.connection.host}`);
     } catch (error) {
-        console.log("connection error",error)
-        process.exit(1)
+        console.log("Connection error:", error);
+        process.exit(1);
     }
-}
+};
+
 export default dbConnect 
