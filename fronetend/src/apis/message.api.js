e77abef991e162a7678494545fe29db11ccc6044
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-16 11:46:36 +0530

    added

---
+++ fronetend/src/apis/message.api.js
@@ -0,0 +1,39 @@
+import axios from "axios"
+
+export const useMessage=()=>{
+ 
+    const sendMessage=async(message)=>{
+       try {
+         const response=await axios.post("/api/v1/message/sendmesage" ,message)
+         return response.data
+       } catch (error) {
+         console.log(error.response.data)
+         return error.response.data
+       }
+    } 
+
+    const deletemessage=async(messageid)=>{
+        try {
+            const response=await axios.post("/api/v1/message/deletemessage",messageid)
+            return response.data
+        } catch (error) {
+            console.log(error.response.data)
+            return error.response.data
+            
+        }
+    }
+
+    const getmessage=async()=>{
+        try {
+            const response=await axios.post("/api/v1/user/getmessage")
+            return response.data
+        } catch (error) {
+            console.log(error.response.data)
+            return error.response.data
+            
+        }
+    }
+    return{
+        sendMessage,deletemessage,getmessage
+    }
+} 
