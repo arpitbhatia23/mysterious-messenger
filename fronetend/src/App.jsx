b7706a68f615a9d4dcca904f8b1d4206b81edfbf
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-17 09:59:48 +0530

    added

--- fronetend/src/App.jsx
+++ fronetend/src/App.jsx
@@ -1,34 +1,23 @@
+import { BackgroundBeams } from './component/ui/background-beams';
 import './App.css';
-import Navbar from './components/Nabvar'; // Assuming there’s a typo in 'Nabvar'
-import bg from './assets/Group 1.svg';
-import Hero from './components/Hero';
-import { useEffect } from 'react';
-import { useAuth } from './apis/auth.api';
+import Nabvar from './components/Nabvar';
+
+import Home from './pages/Home';
+import { BackgroundBeamsWithCollision } from './component/ui/background-beams-with-collision';
 
 function App() {
-  const {login}=useAuth()
-  const user={
-   "email":"aurpitaurpit@gmail.com",
-    "password":"12345678"
-  }
-  useEffect(() => {
-    const fetch=(async()=>{
-      const response= await login(user)
-      console.log(response)
-      
-    })
-    fetch()
-   
-  }, []);
+ 
 
   return (
     <>
       <div
-        className="bg-cover bg-center w-screen h-screen"
-        style={{ backgroundImage: `url(${bg})` }}
+        className="bg-cover bg-center w-screen  bg-gradient-to-b from-black to-gray-800" 
       >
-        <Navbar /> {/* Correcting the typo here */}
-        <Hero />
+ 
+        <Nabvar/>
+     <Home/>
+   
+        
       </div>
     </>
   );
--- fronetend/src/components/MassageBox.jsx
+++ fronetend/src/components/MassageBox.jsx
@@ -1,6 +1,6 @@
+import React from 'react'
 
 const MassageBox = () => {
-
   return (
     <div>
       
