f95f92634c654fa04940ff4a2913f4c501918334
TAMANNA <tamannakarthol320@gmail.com>
2024-09-17 03:36:34 -0700

    store

---
+++ fronetend/src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authslice from './slice'
+const store = configureStore({
+    reducer:{
+        auth:authslice
+    }
+})
+
+export default store
