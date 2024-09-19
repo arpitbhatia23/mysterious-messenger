a25c7fb078f748ce7e0bde12a2a506842ba8f30f
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-19 19:21:40 +0530

    added

--- fronetend/src/components/Messagepage.jsx
+++ fronetend/src/components/Messagepage.jsx
@@ -7,7 +7,7 @@ const Messagepage = () => {
   console.log(userdata)
   return (
     <div className='flex flex-col items-center justify-end  w-screen '>
-      <span className='py-6 text-2xl text-white'>wellcome {userdata.data.fullName}</span>
+      <span className='py-6 text-2xl text-white'>welcome {userdata.data.fullName}</span>
         <Generatelink/>
         <MassageBox/>
 </div>
