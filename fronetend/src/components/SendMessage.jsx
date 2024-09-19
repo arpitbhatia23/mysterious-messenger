e9607f9faa84a2df2cbc307408369ae8abfc9c4b
TAMANNA <tamannakarthol320@gmail.com>
2024-09-18 23:24:15 -0700

    add

--- fronetend/src/components/SendMessage.jsx
+++ fronetend/src/components/SendMessage.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router'
+import { useMessage } from '../apis/message.api.js';
+import Input from './Input.jsx';
+
 
 const SendMessage = () => {
+const receiverid = useParams()
+const [message,setmessage]=useState()
+
+console.log(receiverid);
+const SendMessage = function(message)
+{
+  const {sendMessage}=useMessage()
+
+  sendMessage(message,receiverid)
+  .then((data)=>{console.log(data);
+  })
+}
+
+
   return (
     <div className='text-5xl'>
-        send mesage
       
+      <Input
+      onChange={(e)=>setmessage(e.target.value)}
+      value={message}
+      className='w-96'
+      />
+      <button onClick={()=>SendMessage(message)}>Send</button>
     </div>
   )
 }
