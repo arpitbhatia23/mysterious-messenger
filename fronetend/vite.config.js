86ff4d6c0a8eaa79d2bdb26aced2cb43d83a8ec3
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-09-19 17:18:18 +0530

    added

--- fronetend/vite.config.js
+++ fronetend/vite.config.js
@@ -3,16 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   // Load environment variables
-  const env = loadEnv(mode, process.cwd());
 
   return {
     server: {
       proxy: {
-        "/api": {
-          target: env.VITE_SERVER_URL, // Use loaded environment variable
-          changeOrigin: true,
-          secure: false,
-        },
+        "/api":"https://mysterious-messenger.onrender.com/"
       },
     },
     plugins: [react()],
